3f330e890e17fc141d70d87034f1058f25f403de
Moisés Freitas <63175412+xMDNx@users.noreply.github.com>
2020-05-22 22:18:37 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 function search() {
     var energia = ["Hidrelétrica", "Solar", "Eólica", "Biocombustíveis", "Geotérmica"];
     if(document.getElementById("conteudo").value == energia[0]){
-        window.location.href = "https://xmdnx.github.io/UnipAps/paginas/hidreletrica.html";
+        window.location.href = "https://xmdnx.github.io/UnipAps/energias/hidreletrica.html";
         window.alert("Você será rerirecionado para o conteúdo " + energia[0]);
     }
     if(document.getElementById("conteudo").value == energia[1]){
-        window.location.href = "https://xmdnx.github.io/UnipAps/paginas/termoeletrica.html";
+        window.location.href = "https://xmdnx.github.io/UnipAps/energias/solar.html";
         window.alert("Você será rerirecionado para o conteúdo " + energia[1]);
     }
     if(document.getElementById("conteudo").value == energia[2]){
-        window.location.href = "https://xmdnx.github.io/UnipAps/paginas/hidreletrica11.html";
+        window.location.href = "https://xmdnx.github.io/UnipAps/energias/hidreletrica11.html";
         window.alert("Você será rerirecionado para o conteúdo " + energia[2]);
     }
     if(document.getElementById("conteudo").value == energia[3]){
-        window.location.href = "https://xmdnx.github.io/UnipAps/paginas/hidreletrica11.html";
+        window.location.href = "https://xmdnx.github.io/UnipAps/energias/hidreletrica11.html";
         window.alert("Você será rerirecionado para o conteúdo " + energia[3]);
     }
     if(document.getElementById("conteudo").value == energia[4]){
         window.alert("Você será rerirecionado para o conteúdo " + energia[4]);
-        window.location.href = "https://xmdnx.github.io/UnipAps/paginas/hidreletrica11.html";
+        window.location.href = "https://xmdnx.github.io/UnipAps/energias/hidreletrica11.html";
     }
     
     else{$("#TiposDeEnergiaModal").modal();
