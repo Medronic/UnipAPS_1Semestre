19db2725c834f73a6044fbd8d016d5e61506f290
Moisés Freitas <63175412+xMDNx@users.noreply.github.com>
2020-05-22 22:31:45 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
 function search() {
     var energia = ["Hidrelétrica", "Solar", "Eólica", "Biocombustíveis", "Geotérmica"];
     if(document.getElementById("conteudo").value == energia[0]){
-        window.location.href = "https://xmdnx.github.io/UnipAps/energias/hidreletrica.html";
+        location.replace("https://xmdnx.github.io/UnipAps/energias/hidreletrica.html");
         window.alert("Você será rerirecionado para o conteúdo " + energia[0]);
     }
     if(document.getElementById("conteudo").value == energia[1]){
-        window.location.href = "https://xmdnx.github.io/UnipAps/energias/solar.html";
+        location.replace("https://xmdnx.github.io/UnipAps/energias/solar.html");
         window.alert("Você será rerirecionado para o conteúdo " + energia[1]);
     }
     if(document.getElementById("conteudo").value == energia[2]){
-        window.location.href = "https://xmdnx.github.io/UnipAps/energias/hidreletrica11.html";
+        location.replace("https://xmdnx.github.io/UnipAps/energias/eolica.html");
         window.alert("Você será rerirecionado para o conteúdo " + energia[2]);
     }
     if(document.getElementById("conteudo").value == energia[3]){
-        window.location.href = "https://xmdnx.github.io/UnipAps/energias/hidreletrica11.html";
+        location.replace("https://xmdnx.github.io/UnipAps/energias/hidreletrica11.html");
         window.alert("Você será rerirecionado para o conteúdo " + energia[3]);
     }
     if(document.getElementById("conteudo").value == energia[4]){
+        location.replace("https://xmdnx.github.io/UnipAps/energias/hidreletrica11.html");
         window.alert("Você será rerirecionado para o conteúdo " + energia[4]);
-        window.location.href = "https://xmdnx.github.io/UnipAps/energias/hidreletrica11.html";
     }
     
-    else{$("#TiposDeEnergiaModal").modal();
+    /*else{$("#TiposDeEnergiaModal").modal();
     document.getElementById("energia_tags").innerHTML = energia;
     window.alert("Ops, o conteúdo que você está procurando não existe! Mas após clicar em ok, irá ser exibido uma nova janela para lhe orientar."); 
-    }
+    }*/
     
 }
 
